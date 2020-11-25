6bd92216af4c385d86d0037bb75b47964e4b879d
Aaron <aaronhuange@gmail.com>
2020-11-25 13:46:23 +0800

    适配安卓8.0后，服务不能在onDestroy且为后台的时候重启

--- androidapp/androidmodule/src/main/java/com/pk/mp/androidmodule/server/impl/WsService.java
+++ androidapp/androidmodule/src/main/java/com/pk/mp/androidmodule/server/impl/WsService.java
@@ -1,8 +1,14 @@
 package com.pk.mp.androidmodule.server.impl;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
+import com.pk.mp.androidmodule.R;
 import com.pk.mp.androidmodule.WsClient;
 import com.pk.mp.androidmodule.server.BaseService;
 import com.pk.mp.androidmodule.utils.AppUtil;
@@ -10,11 +16,26 @@ import com.pk.mp.androidmodule.utils.AppUtil;
 import androidx.annotation.Nullable;
 
 public class WsService extends BaseService {
+    private String CHANNEL_ID_STRING = "com.pk.mp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //适配8.0service
+        NotificationManager notificationManager = (NotificationManager) getApplication().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel mChannel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            mChannel = new NotificationChannel(CHANNEL_ID_STRING, getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(mChannel);
+            Notification notification = new Notification.Builder(getApplicationContext(), CHANNEL_ID_STRING).build();
+            startForeground(1, notification);
+        }
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         WsClient.get(getApplicationContext());
-//        AppUtil.startApp(getApplicationContext(), "org.esbuilder.mp.dfh.sit");
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -27,7 +48,11 @@ public class WsService extends BaseService {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Intent intent=new Intent(WsService.this,WsService.class);
-        startService(intent);
+        Intent intent = new Intent(WsService.this, WsService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        } else {
+            startService(intent);
+        }
     }
 }
--- androidapp/app/src/main/java/com/pk/androidapp/MainActivity.java
+++ androidapp/app/src/main/java/com/pk/androidapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.pk.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.pk.mp.androidmodule.server.impl.WsService;
